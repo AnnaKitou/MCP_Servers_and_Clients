f39e14f4770f75f0fea968c97fd903f772da785c
kitouanna@gmail.com <kitouanna@gmail.com>
2026-01-30 23:31:19 +0200

    deployment

---
+++ MCP_SERVER_DEEPDIVE_DEPLOYMENT/build/lib/mcpserver/deployment.py
@@ -0,0 +1,13 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an mcp server
+mcp=FastMCP("Demo")
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """
+    Adds two numbers together.
+    """
+    return a + b
